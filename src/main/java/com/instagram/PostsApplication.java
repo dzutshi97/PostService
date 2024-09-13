59d8f28db6c5749da01e4ccd8ee042ba19bbb2a3
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-14 02:06:25 +0530

    Update PostsApplication.java

--- src/main/java/com/instagram/PostsApplication.java
+++ src/main/java/com/instagram/PostsApplication.java
@@ -13,6 +13,8 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import reactor.core.publisher.Hooks;
+
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
@@ -64,6 +66,7 @@ public class PostsApplication {
 	}
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation(); //needed to generate trace id and span id
 		SpringApplication.run(PostsApplication.class, args);
 	}
 }
