5ff017d67559d28588e327681d1b0a278ea845db
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-14 02:13:33 +0530

    Update CommentService.java

--- src/main/java/com/instagram/service/CommentService.java
+++ src/main/java/com/instagram/service/CommentService.java
@@ -2,6 +2,8 @@ package com.instagram.service;
 
 import com.instagram.models.Comment;
 import com.instagram.repository.CommentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,16 @@ public class CommentService {
 
     @Autowired
     CommentRepository commentRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CommentService.class);
+
 
     public List<Comment> fetchComments(String postId, int page, int pageSize){
+        logger.info("fetch comments");
         return commentRepository.fetchCommentsByPostId(postId, page, pageSize);
     }
 
     public String addComment(String postId, String comment) throws Exception {
+        logger.info("add comment");
         Comment cmnt = new Comment();
         cmnt.setCommentId(UUID.randomUUID().toString());
         cmnt.setComment(comment);
@@ -28,6 +34,7 @@ public class CommentService {
     }
 
     public void deleteComment(String postId, String commentId) throws Exception {
+        logger.info("delete comment");
         commentRepository.deleteComment(postId,commentId);
     }
 
