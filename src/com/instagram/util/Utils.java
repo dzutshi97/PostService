75e205a5c78d15d27e96668eed5c7ed542859e66
dzutshi97 <103752774+dzutshi97@users.noreply.github.com>
2024-09-13 19:33:14 +0530

    Add files via upload

---
+++ src/com/instagram/util/Utils.java
@@ -0,0 +1,37 @@
+package com.instagram.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class Utils {
+
+    public String readFile(String filename) {
+        String result = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            result = sb.toString();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+//    public List<Player> convertJsonToList(String json) throws IOException {
+//        return objectMapper.readValue(json, new TypeReference<List<Player>>() {});
+//    }
+}
